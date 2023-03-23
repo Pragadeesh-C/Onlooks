22b9388883abda23f02ab6123d32505c8155c7ba
Belgin <Belgin.confidential@gmail.com>
2023-03-23 11:48:17 +0530

    Add Files

--- Screens/Attendance.js
+++ Screens/Attendance.js
@@ -1,28 +1,20 @@
-import { StyleSheet, Text, View,FlatList } from 'react-native'
-import CheckBox from '@react-native-community/checkbox'
-import React, { useState,useEffect } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Attendance = () => {
-  
-  return (
-    <SafeAreaView style={styles.container}>
-    
+  return <SafeAreaView style={styles.container}></SafeAreaView>;
+};
 
-    </SafeAreaView>
-  )
-}
-
-
-export default Attendance
+export default Attendance;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
   },
-  list:{
-    flex:1,
-    padding:8,
+  list: {
+    flex: 1,
+    padding: 8,
   },
-
-})
+});
--- Screens/Login.js
+++ Screens/Login.js
@@ -1,61 +1,108 @@
-import { StyleSheet, Text, TouchableOpacity, View ,TextInput} from 'react-native'
-import { useState } from 'react'
-import auth from '@react-native-firebase/auth'
-import Signup from './Signup'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from 'react-native';
+import {useState} from 'react';
+import auth from '@react-native-firebase/auth';
+import Signup from './Signup';
+import firestore from '@react-native-firebase/firestore';
+import React from 'react';
 
-const Login = ( {navigation}) => {
-  const [emailID,setemailID] = useState('')
-  const [password,setpassword] = useState('')
-  
-  const LoginAuth = async() => {
-    await auth().signInWithEmailAndPassword(emailID,password)
-    navigation.navigate('Attendance',{name:'Attendance'})
-    console.log(auth().currentUser?.email)
-  }
+const Login = ({navigation}) => {
+  const [userid, setuserid] = useState('');
+  const [password, setpassword] = useState('');
+
+  const LoginAuth = async () => {
+    firestore()
+      .collection('Users')
+      .where('id', 'in', [userid])
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(snapshot => {
+          if (snapshot.role == 'student') {
+            navigation.navigate('');
+          } else {
+            navigation.navigate('');
+          }
+        });
+      });
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.card}>
-
-      <Text style={{ fontSize:24,marginTop:100,textAlign:"center",justifyContent:"center",}}>Login</Text>
-      <TextInput placeholder='Email' value={emailID} onChangeText={(Text)=>setemailID(Text)}  style={{ height: 40, borderColor: 'gray', borderWidth: 1,marginTop:70,marginHorizontal:10, }}/>
-      <TextInput placeholder='Password' value={password} onChangeText={(Text)=>setpassword(Text)} secureTextEntry={true} style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginTop:40,marginHorizontal:10, }}/>
-      <View style={{flexDirection:"row",}}>
-
-      <TouchableOpacity style={styles.button} onPress={LoginAuth} >
-        <Text style={{color:"white",fontSize:15,} }>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={()=>navigation.navigate('Signup',{name:"Signup"})} >
-        <Text style={{color:"white",fontSize:15,} }>Signup</Text>
-      </TouchableOpacity>
-      </View>
+        <Text
+          style={{
+            fontSize: 24,
+            marginTop: 100,
+            textAlign: 'center',
+            justifyContent: 'center',
+          }}>
+          Login
+        </Text>
+        <TextInput
+          placeholder="Email"
+          value={userid}
+          onChangeText={Text => setuserid(Text)}
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            marginTop: 70,
+            marginHorizontal: 10,
+          }}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={Text => setpassword(Text)}
+          secureTextEntry={true}
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            marginTop: 40,
+            marginHorizontal: 10,
+          }}
+        />
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity style={styles.button} onPress={LoginAuth}>
+            <Text style={{color: 'white', fontSize: 15}}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Signup', {name: 'Signup'})}>
+            <Text style={{color: 'white', fontSize: 15}}>Signup</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     // backgroundColor:"",
     // alignItems:"center",
-    
   },
-  button:{
-    height:50,
-    width:200,
-    backgroundColor:"blue",
-    alignItems:"center",
-    justifyContent:"center",
-    borderRadius:20,
-    marginLeft:5,
-    marginTop:40,
+  button: {
+    height: 50,
+    width: 200,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    marginLeft: 5,
+    marginTop: 40,
   },
-  card:{
+  card: {
     // justifyContent:"center",
     // alignItems:"center",
-  }
-})
+  },
+});
